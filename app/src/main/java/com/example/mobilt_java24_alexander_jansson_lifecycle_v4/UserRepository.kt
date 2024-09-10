c7b713dae74388fb6c23e49e23dc47672ccfc2e7
Alexander Jansson <alexanderjson@hotmail.com>
2024-09-11 00:22:31 +0200

    added singleton+lazy init for firebase connection

--- app/src/main/java/com/example/mobilt_java24_alexander_jansson_lifecycle_v4/UserRepository.kt
+++ app/src/main/java/com/example/mobilt_java24_alexander_jansson_lifecycle_v4/UserRepository.kt
@@ -1,6 +1,7 @@
 package com.example.mobilt_java24_alexander_jansson_lifecycle_v4
 
 import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
@@ -8,9 +9,15 @@ import kotlinx.coroutines.withContext
 
 class UserRepository {
 
-    private val db = Firebase.firestore
+    object initFirebase{
+        val firestoreInstance: FirebaseFirestore by lazy {
+            FirebaseFirestore.getInstance()
+        }
+    }
+
         suspend fun getUsers() {
             return withContext(Dispatchers.IO){
+                val db = initFirebase.firestoreInstance
                 val usersCollection = db.collection("users")
                 // testing
                 usersCollection.get()
@@ -19,7 +26,7 @@ class UserRepository {
                             val username = document.getString("username")
                             val password = document.getString("password")
 
-                            Log.d("Firestore working with coroutines!!!!", "Username: $username, Password: $password")
+                            Log.d("FIREBASEdsdskkjlsdZ", "Username: $username, Password: $password")
                         }
                     }
                     .addOnFailureListener { exception ->
