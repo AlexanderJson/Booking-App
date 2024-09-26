8ef56d1fbae6e15f454d20ffb934f90a54fa4db1
Alexander Jansson <alexanderjson@hotmail.com>
2024-09-26 06:22:00 +0200

    added logic for navbar

--- app/src/main/java/com/example/mobilt_java24_alexander_jansson_lifecycle_v4/HomepageActivity.kt
+++ app/src/main/java/com/example/mobilt_java24_alexander_jansson_lifecycle_v4/HomepageActivity.kt
@@ -9,7 +9,9 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.launch
+
 
 class HomepageActivity : AppCompatActivity() {
 
@@ -17,6 +19,33 @@ class HomepageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val text = findViewById<TextView>(R.id.textView2)
+        val bottomNavbar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavbar.setOnItemSelectedListener{ item ->
+            when (item.itemId){
+                R.id.nav_home -> {
+                    val intent = Intent(this, HomepageActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_profile -> {
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.nav_schedule -> {
+                    val intent = Intent(this, CalendarActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+
+
+        }
 
         //val calendarBtn = findViewById<Button>(R.id.calendarBtn)
 
@@ -24,6 +53,28 @@ class HomepageActivity : AppCompatActivity() {
         calendarBtn.setOnClickListener(View.OnClickListener {
             intent = Intent(this, CalendarActivity::class.java)
             startActivity(intent)
-        })*/
+        })
+
+
+
+
+
+            when (item.itemId){
+                R.id.nav_home -> {
+                    intent = Intent(this, HomepageActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_schedule -> {
+                    intent = Intent(this, CalendarActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_profile -> {
+                    intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+
+        */
 }}
 
